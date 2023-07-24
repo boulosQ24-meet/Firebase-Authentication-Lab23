7cd861b657dc8124f45339fc2ba227a7e3e96e39
boulosQ24-meet <boulosbqurbashi@gmail.com>
2023-07-24 12:15:42 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -6,6 +6,23 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
+Config = {
+  "apiKey": "AIzaSyCvXSClwtv85sbJgxNJq1Gt9yXPWIdSmbY",
+  "authDomain": "meettesty2boulos.firebaseapp.com",
+  "projectId": "meettesty2boulos",
+  "storageBucket": "meettesty2boulos.appspot.com",
+  "messagingSenderId": "997430567535",
+  "appId": "1:997430567535:web:8d762221f7e033c7d8e39a",
+  "measurementId": "G-MH1LKJHK9P"
+   "databaseURL": ""
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def signin():
     return render_template("signin.html")
@@ -13,6 +30,10 @@ def signin():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.methods == "POST":
+        try:
+            email = request.form['email']
+            password = request.form['password']
     return render_template("signup.html")
 
 
