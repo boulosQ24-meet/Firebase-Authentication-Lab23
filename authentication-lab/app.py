251841966e89d8e450e0b54622b34f9c54491453
boulosQ24-meet <boulosbqurbashi@gmail.com>
2023-07-24 19:06:21 +0300

    finished

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -7,14 +7,14 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 Config = {
-    "apiKey": "AIzaSyCvXSClwtv85sbJgxNJq1Gt9yXPWIdSmbY",
-    "authDomain": "meettesty2boulos.firebaseapp.com",
-    "projectId": "meettesty2boulos",
-    "storageBucket": "meettesty2boulos.appspot.com",
-    "messagingSenderId": "997430567535",
-    "appId": "1:997430567535:web:8d762221f7e033c7d8e39a",
-    "measurementId": "G-MH1LKJHK9P",
-    "databaseURL": ""
+    'apiKey': "AIzaSyCvXSClwtv85sbJgxNJq1Gt9yXPWIdSmbY",
+    'authDomain': "meettesty2boulos.firebaseapp.com",
+    'projectId': "meettesty2boulos",
+    'storageBucket': "meettesty2boulos.appspot.com",
+    'messagingSenderId': "997430567535",
+    'appId': "1:997430567535:web:8d762221f7e033c7d8e39a",
+    'measurementId': "G-MH1LKJHK9P",
+    "databaseURL": "https://meettesty2boulos-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 firebase = pyrebase.initialize_app(Config)
@@ -25,11 +25,12 @@ auth = firebase.auth()
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    error=''
     if request.method == "POST":
         email = request.form['email']
         password = request.form['password']
         try:
-            login_session['user'] = auth.sign_in_with_email_and_password('email', 'password')
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
             return redirect(url_for('add_tweet'))
         except:
             error = "error habibi"
@@ -38,11 +39,12 @@ def signin():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    error=''
     if request.method == "POST":
         email = request.form['email']
         password = request.form['password']
         try:
-            login_session['user'] = auth.create_user_with_email_and_password('email', 'password')
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
             return redirect(url_for('add_tweet'))
         except:
             error = "error habibi"
